33612234386ad1b931e94df28a43eecfc52a03c3
Roshmita-2002 <roshmitachoudhury@gmail.com>
2026-03-12 10:10:09 +0530

    Comments

---
+++ src/COM/SUM/demo.java
@@ -0,0 +1,13 @@
+package COM.SUM;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=32;
+		System.out.println(a+b);
+
+	}
+
+}
